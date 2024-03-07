7370d0754df803ff0c311111baaef0f3107437d5
huypip1234 <huy123nn69hz@gmail.com>
2024-03-07 14:19:46 +0700

    fuckkk

--- api/database/database.js
+++ api/database/database.js
@@ -3,17 +3,17 @@ import mongoose from "mongoose";
 const connect = async (req, res) => {
   try {
     console.log("Connecting...");
-    /* let connection = await mongoose.connect(
+    let connection = await mongoose.connect(
       "mongodb+srv://huydeptrai:14120042003bB@cluster0.hy9v3jy.mongodb.net/haisancophuong?retryWrites=true&w=majority"
-    ); */
+    );
     console.log("Connected to database!");
     return connection; //return void
   } catch (error) {
-    /* res.status(500).json({
+    res.status(500).json({
       message: "Error Server cmnr",
-    }); */
+    });
     console.log("Error cmnr: " + error);
-    //throw new Error("Fail to connect to DB cmnr");
+    throw new Error("Fail to connect to DB cmnr");
   }
 };
 
